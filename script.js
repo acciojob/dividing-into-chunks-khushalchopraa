a0c16afb6dd937c9cfac7bf326ef1e2370dc381e
khushalchopraa <advkhushal97@gmail.com>
2024-07-18 12:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  // def divide(arr, n):
+    result = []
+    current_subarray = []
+    current_sum = 0
+
+    for num in arr:
+        if current_sum + num <= n:
+            current_subarray.append(num)
+            current_sum += num
+        else:
+            result.append(current_subarray)
+            current_subarray = [num]
+            current_sum = num
+
+    if current_subarray:
+        result.append(current_subarray)
+
+    return result
+		 
+		
+	}
+	
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+//const n = prompt("Enter n: ");
+//alert(JSON.stringify(divide(arr, n)));
